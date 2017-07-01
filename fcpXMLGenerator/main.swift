bc273acad3cd15a5d8efe2761a0b40a9d310526f
Damiaan Dufaux <damiaan@dpro.be>
2017-07-01 19:46:55 +0200

    Initial Commit

---
+++ fcpXMLGenerator/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  fcpXMLGenerator
+//
+//  Created by Damiaan on 1/07/17.
+//  Copyright © 2017 Damiaan Dufaux. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
